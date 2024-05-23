614e530289d180dc37c653927818b69b8e32fef9
Herbert Lange <github@pietaetskirsche.de>
2024-05-23 14:37:52 +0200

    add rendering of a color bar as legend and grey circles to border the disk (making black areas show up more clearly)

--- app/Main.hs
+++ app/Main.hs
@@ -23,13 +23,24 @@ import GHC.Float
 colorMap :: [Colour Double]
 colorMap =
   black:[sRGB24 r g b | i <- [0..255], let hsvColor = hsv (fromIntegral $ i+45 `mod` 360) 1 1 :: RGB Double, let RGB r g b = fmap (round . (255*)) hsvColor :: RGB Word8]
+
+-- | Draws the color map to be included as a the legend
+drawColorMap :: Diagram B
+drawColorMap =
+  let
+    maxColor = length colorMap -1
+    colorBar = hcat [rect 4 2 # fc (colorMap !! colorIndex) # lw none | colorIndex <- [0..maxColor]] :: Diagram B
+    labelText = hsep 49 [text (show index) # fc (colorMap !! index) # fontSizeL 10 <> if index == 0 then rect 15 15 # lw none # bg white else rect 15 40 # lw none | index <- [0..length colorMap -1], index `mod` 16 == 0] :: Diagram B
+  in
+    colorBar === labelText
+    
+-- | Reads a single track, i.e. number of sectors * 256. The number of sectors depends on the track index
 readTrack :: Int -> Handle -> IO BS.ByteString
 readTrack track handle
   | 1 <= track && track <= 17 = BS.hGet handle $ 21 * 256
   | 18 <= track && track <= 24 = BS.hGet handle $ 19 * 256
   | 25 <= track && track <= 30 = BS.hGet handle $ 18 * 256
   | 31 <= track && track <= 40 = BS.hGet handle $ 17 * 256
-
 
 readDisk :: Handle -> IO [BS.ByteString]
 readDisk handle = sequence [readTrack track handle | track <- [1..40]]
@@ -57,7 +68,7 @@ drawDisk tracks =
     -- The list of tracks together with track numbers
     diskData = zip [1..] tracks :: [(Word8, BS.ByteString)]
   in
-    foldl atop  (circle 5 # lw none # fc white) $ concat [drawTrack radius trackWidth trackId trackData | (trackId,trackData) <- diskData]
+    drawColorMap === ((circle 810 # lw none # fc grey) `beneath` (foldl atop  (circle 5 # lw none # fc white <> circle 95 # lw none # fc grey) $ concat [drawTrack radius trackWidth trackId trackData | (trackId,trackData) <- diskData]))
 
 -- main' =
 --   renderSVG "ddt.svg" absolute =<<
